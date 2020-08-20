38c363380442b26dfe1a5db8062bbb896d9ed8c4
Charles Lai <charlesangeloplai@gmail.com>
2020-08-20 18:14:58 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -8,14 +8,4 @@
 
 def greeting
   phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
 end
