5b998c44c1eb1878964458df3c5413d24201df2d
Charles Lai <charlesangeloplai@gmail.com>
2020-08-20 18:13:00 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,11 @@
 # Use this space to code along with the readme
+## Objectives
+
+# - Describe how methods can define new routines and procedures for our code.
+# - Define a method with the `def` keyword, supply the method's body, and close
+#  the method definition with the `end` keyword.
+# - Invoke a method by calling it by name.
+
 
 phrase = "Hello World!"
 puts phrase
